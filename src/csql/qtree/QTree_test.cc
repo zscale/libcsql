07c4bedd14ffeb63c507cb781e42d8256bf73713
Paul Asmuth <paul@paulasmuth.com>
2015-12-19 15:56:57 +0100

    QTree_test stub

---
+++ src/csql/qtree/QTree_test.cc
@@ -0,0 +1,50 @@
+/**
+ * This file is part of the "libcsql" project
+ *   Copyright (c) 2015 Paul Asmuth
+ *
+ * FnordMetric is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License v3.0. You should have received a
+ * copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+#include <stx/stdtypes.h>
+#include <stx/exception.h>
+#include <stx/wallclock.h>
+#include <stx/test/unittest.h>
+#include "csql/runtime/defaultruntime.h"
+#include "csql/qtree/SequentialScanNode.h"
+#include "csql/qtree/ColumnReferenceNode.h"
+#include "csql/qtree/CallExpressionNode.h"
+#include "csql/qtree/LiteralExpressionNode.h"
+#include "csql/CSTableScanProvider.h"
+
+using namespace stx;
+using namespace csql;
+
+UNIT_TEST(QTreeTest);
+
+TEST_CASE(QTreeTest, TestExtractSimpleConstraint, [] () {
+  auto runtime = Runtime::getDefaultRuntime();
+
+  auto estrat = mkRef(new DefaultExecutionStrategy());
+  estrat->addTableProvider(
+      new CSTableScanProvider(
+          "testtable",
+          "src/csql/testdata/testtbl.cst"));
+
+  String query = "select 1 from testtable;";
+  csql::Parser parser;
+  parser.parse(query.data(), query.size());
+
+  auto qtree_builder = runtime->queryPlanBuilder();
+  auto qtrees = qtree_builder->build(
+      parser.getStatements(),
+      estrat->tableProvider());
+
+  EXPECT_EQ(qtrees.size(), 1);
+  auto qtree = qtrees[0];
+  EXPECT_TRUE(dynamic_cast<SequentialScanNode*>(qtree.get()) != nullptr);
+  auto seqscan = qtree.asInstanceOf<SequentialScanNode>();
+
+  EXPECT_EQ(seqscan->tableName(), "testtable");
+});
