0e76138e4aac8ff2bbc16fed6633a499bb102028
Paul Asmuth <paul@paulasmuth.com>
2016-01-05 13:51:10 +0100

    fix subquery SELECT wildcard/asterisk expansion

--- src/csql/runtime/queryplanbuilder.cc
+++ src/csql/runtime/queryplanbuilder.cc
@@ -1184,9 +1184,9 @@ QueryTreeNode* QueryPlanBuilder::buildSubqueryTableReference(
   Vector<RefPtr<SelectListNode>> select_list_expressions;
   for (const auto& select_expr : select_list->getChildren()) {
     if (*select_expr == ASTNode::T_ALL) {
-      for (const auto& col : subquery_tbl->allColumns()) {
-        auto sl = new SelectListNode(new ColumnReferenceNode(col.qualified_name));
-        sl->setAlias(col.short_name);
+      for (const auto& col : subquery_tbl->outputColumns()) {
+        auto sl = new SelectListNode(new ColumnReferenceNode(col));
+        sl->setAlias(col);
         QueryTreeUtil::resolveColumns(sl->expression(), resolver);
         select_list_expressions.emplace_back(sl);
       }
